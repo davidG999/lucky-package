d82c44098d0954677c9d871ac17f322a19c7ea88
davidg999 <davyd.arutiunian@gmail.com>
2025-04-25 23:03:25 +0300

    feat: add `--unsafe` option

--- bin/index.js
+++ bin/index.js
@@ -1,28 +1,31 @@
 #!/usr/bin/env node
 const allNames = require('all-the-package-names');
 const { execSync } = require('node:child_process');
+const argv = require('minimist')(process.argv.slice(2));
 
-function isPackagePotentiallyDangerous(packageName) {
-  try {
-    const scriptsJson = execSync(`npm view ${packageName} scripts --json`, { encoding: 'utf8' });
-    if (!scriptsJson || scriptsJson.trim() === 'null') {
-      return false;
+(() => {
+  function isPackagePotentiallyDangerous(packageName) {
+    try {
+      const scriptsJson = execSync(`npm view ${packageName} scripts --json`, {encoding: 'utf8'});
+      if (!scriptsJson || scriptsJson.trim() === 'null') {
+        return false;
+      }
+      const scripts = JSON.parse(scriptsJson);
+      return scripts.preinstall || scripts.install || scripts.postinstall;
+    } catch {
+      return true;
     }
-    const scripts = JSON.parse(scriptsJson);
-    return scripts.preinstall || scripts.install || scripts.postinstall;
-  } catch {
-    return true;
   }
-}
 
-let randomName;
-while (true) {
-  const candidate = allNames[Math.floor(Math.random() * allNames.length)];
-  if (!isPackagePotentiallyDangerous(candidate)) {
-    randomName = candidate;
-    break;
+  let randomName;
+  while (true) {
+    const candidate = allNames[Math.floor(Math.random() * allNames.length)];
+    if (argv.unsafe || !isPackagePotentiallyDangerous(candidate)) {
+      randomName = candidate;
+      break;
+    }
   }
-}
 
-console.log(`🎲 Installing random package: ${randomName}`);
-execSync(`npm install ${randomName}`, { stdio: 'inherit' });
+  console.log(`🎲 Installing random package: ${randomName}`);
+  execSync(`npm install ${randomName}`, {stdio: 'inherit'});
+})()
